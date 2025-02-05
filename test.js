dd8880f6c1fd5d27091a2e71e10099b0c26c67f1
pancake0003 <songyuemmm@gmail.com>
2025-02-05 15:11:33 -0500

    trying vercel...

--- test.js
+++ test.js
@@ -1,42 +1,27 @@
 const express = require('express');
 const path = require('path');
-const { createServer } = require('@vercel/node');
+
 const app = express();
-// const port = 3000;
 
-// in middleware, the order matters...
-// check the hour first!
-app.use(workingHours);
-// serving our static files like always
-app.use(express.static('public'));
-
-app.listen(port, () => {
-  console.log('Server listening at http://localhost:3000');
-});
-
-// our custom middleware function
 function workingHours(req, res, next) {
+  if (req.path === '/Yuemeng_Song_Resume.pdf') {
+    return next();
+  }
 
   const currentTime = new Date();
   const currentHour = currentTime.getHours();
 
   const normalBusinessHours = {
-    // 24 hour time
-    open: 11,
-    close: 3,
+    open: 11, 
+    close: 3, 
   };
 
-  if (currentHour >= normalBusinessHours.open ||
-    currentHour <= normalBusinessHours.close) {
+  if (currentHour >= normalBusinessHours.open || currentHour < normalBusinessHours.close) {
     console.log('Open!');
-    // req.url = 'public/Yuemeng_Song_Resume.pdf';
-    res.sendFile(path.join(__dirname, 'public', 'Yuemeng_Song_Resume.pdf'));
-    // next();
-  } 
-  else {
-    // req.url = 'denied.html';
+    next();
+  } else {
+    console.log('Closed 🔒');
     res.sendFile(path.join(__dirname, 'public', 'denied.html'));
-    // next();
   }
 }
 
