acf3182662d073d3d56c8339d8fce251f0e58e3d
pancake0003 <songyuemmm@gmail.com>
2025-02-05 15:47:08 -0500

    still vercel

--- api/index.js
+++ api/index.js
@@ -32,4 +32,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
+app.get('/api', (req, res) => {
+  res.json({ message: 'API is working!' });
+});
+
 module.exports = app;
